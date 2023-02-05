5dc83c6d870dbde2cb4a0eb5e090a71966e953b9
Jenn Dell <64788812+maddhattress@users.noreply.github.com>
2023-02-05 10:47:07 -0500

    Initial

---
+++ src/simple/HelloWorld.java
@@ -0,0 +1,17 @@
+package simple;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("bye");
+		System.out.println("hi");
+		System.out.println("stuff");
+		//System.out.println("things");
+	}
+
+}
